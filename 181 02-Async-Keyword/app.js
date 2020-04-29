86be072234ccdf2ee1557038740641ce40eaa246
fouad97 <loukrichi21@gmail.com>
2020-04-29 11:17:58 +0100

    added Async await section

--- 181 02-Async-Keyword/app.js
+++ 181 02-Async-Keyword/app.js
@@ -1,4 +1,14 @@
 async function getPlanets() {
-	const planets = axios.get("")
+	try {
+		const data = await fetch("https://swapi.dev/api/planets/");
+		const readData = await data.json();
+		const { results } = readData;
+		console.log(results);
+	} catch (e) {
+		console.log("Oops!", e);
+	}
 }
 
+getPlanets().catch((err) => {
+	console.log("Oops!", err);
+});
